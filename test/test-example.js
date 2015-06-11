36dbe94f5e18da5b4932d0f85364860656fa0dd4
Marko Grešak <marko@gresak.io>
2015-06-11 16:52:55 +0200

    add example test to make travis pass

---
+++ test/test-example.js
@@ -0,0 +1,11 @@
+'use strict';
+var chai = require('chai');
+var sinon = require('sinon');
+chai.should();
+
+describe('description', function () {
+  it('should pass', function (done) {
+    true.should.be.true;
+    done();
+  });
+});
